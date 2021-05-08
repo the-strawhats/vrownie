1578c16ee0b247dbfcfb789ae98d5725902c9fa8
Gabriel Escame <gabriel.escame@gmail.com>
2021-05-08 19:55:00 -0300

    feat: adding iconMapper

---
+++ src/assets/icons.ts
@@ -0,0 +1,19 @@
+import vegan from './vegan.svg'
+import eco from './eco.svg'
+import shop from './shop.svg'
+import hamburguer from './hamburguer.svg'
+import whatsapp from './whatsapp.svg'
+import instagram from './instagram.svg'
+import facebook from './facebook.svg'
+
+const iconMapper = {
+  vegan: vegan,
+  eco: eco,
+  shop: shop,
+  hamburguer: hamburguer,
+  whatsapp: whatsapp,
+  instagram: instagram,
+  facebook: facebook
+}
+
+export default iconMapper
