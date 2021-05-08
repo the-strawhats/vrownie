c42c398ca109845c56e35356c3d4ed6a97ad20a7
Gabriel Escame <gabriel.escame@gmail.com>
2021-05-08 19:54:39 -0300

    feat: adding svgr

--- next.config.js
+++ next.config.js
@@ -3,3 +3,15 @@ const withImages = require('next-images')
 module.exports = withImages({
   esModule: true
 })
+
+module.exports = {
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ['@svgr/webpack']
+    })
+
+    return config
+    s
+  }
+}
