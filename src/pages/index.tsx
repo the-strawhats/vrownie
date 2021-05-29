63e188fb8c5872eef8558e5f5876e53e0def12d4
Gabriel Escame <gabriel.escame@gmail.com>
2021-05-29 12:02:37 -0300

    feat: HomeWrapper

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,16 +2,21 @@ import Head from 'next/head'
 import React from 'react'
 import HomeView from '../view/Home'
 import Nav from '../components/Nav'
+import styled from 'styled-components'
+
+const HomeWrapper = styled.div` 
+  overflow: hidden;
+`
 
 const Home: React.FC = () => {
   return (
-    <>
+    <HomeWrapper>
       <Head>
         <title>Vrownie</title>
       </Head>
       <Nav />
       <HomeView />
-    </>
+    </HomeWrapper>
   )
 }
 
