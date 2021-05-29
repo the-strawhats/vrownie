69e73ba84322e2c5091a275aad0d41a702305886
Gabriel Escame <gabriel.escame@gmail.com>
2021-05-29 18:46:12 -0300

    feat: removing home interface

--- src/view/Home/index.tsx
+++ src/view/Home/index.tsx
@@ -2,16 +2,16 @@ import React from 'react'
 import Hero from './components/Hero'
 import ProductsSection from './components/ProductsSection'
 import StorySection from './components/StorySection'
+import Footer from './components/Footer'
 import { Container } from '../../styles/global'
 
-interface HomeInterface {}
-
-const Home: React.FC<HomeInterface> = ({}) => {
+const Home: React.FC = () => {
   return (
     <Container>
       <Hero />
       <ProductsSection />
       <StorySection />
+      <Footer />
     </Container>
   )
 }
