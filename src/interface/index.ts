42562fa4d4f73302fdbd98d7b1a7579b875ea44d
Gabriel Escame <gabriel.escame@gmail.com>
2021-12-30 14:26:27 -0300

    feat: adding adress interface

--- src/interface/index.ts
+++ src/interface/index.ts
@@ -8,3 +8,14 @@ export interface CartItem {
 export interface AnimatedElementInterface {
   isAnimated: boolean
 }
+
+export interface AddressPayloadInterface {
+  street?: string
+  neighborhood?: string
+  number?: string
+  complement?: string
+  station?: string
+  observation?: string
+}
+
+export type DeliveryType = 'Endereço' | 'Metrô' | 'Retirar' | ''
