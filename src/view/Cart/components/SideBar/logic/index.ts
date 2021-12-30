b1c0b8c3e700d40b4fc5adb12e849fa805054252
Gabriel Escame <gabriel.escame@gmail.com>
2021-12-30 14:26:59 -0300

    feat: addinf adress text to hadnleOrderNow

--- src/view/Cart/components/SideBar/logic/index.ts
+++ src/view/Cart/components/SideBar/logic/index.ts
@@ -1,5 +1,10 @@
+import { DeliveryType } from '@interface/index'
 import composer from '@utils/composer'
-import { getStorageItem, cartListToOrder } from '@utils/index'
+import {
+  getStorageItem,
+  cartListToOrder,
+  deliveryFormToOrder
+} from '@utils/index'
 import React, { useState } from 'react'
 
 const initialContentForm = {
@@ -12,23 +17,24 @@ const initialContentForm = {
 }
 
 const useSidebar = () => {
-  const [currentOption, setCurrentOption] = useState('')
+  const [currentOption, setCurrentOption] = useState<DeliveryType>('')
   const [contentForm, setContentForm] = useState(initialContentForm)
 
   const handleOnChange = (model: string) => (
     event: React.FormEvent<HTMLInputElement>
-    ) => {
-      const form = contentForm
-      const { value } = event.currentTarget
-      form[model] = value
-      setContentForm(form)
+  ) => {
+    const form = contentForm
+    const { value } = event.currentTarget
+    form[model] = value
+    setContentForm(form)
   }
 
   const handleOrderNow = () => {
     const cartList = getStorageItem('cart')
     const orderText = cartListToOrder(cartList)
+    const addressText = deliveryFormToOrder(currentOption, contentForm)
     window.open(
-      `https://api.whatsapp.com/send?phone=5511967389394&text=${orderText}`
+      `https://api.whatsapp.com/send?phone=5511967389394&text=${orderText}%0a%0a${addressText}`
     )
   }
   return {
