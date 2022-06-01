381fe70be05ed527cba3e41e55877e325f02b703
Gabriel Escame <gabriel.escame@gmail.com>
2022-05-31 22:55:06 -0300

    feat: adding AnchorCask

--- src/view/Home/components/Footer/index.tsx
+++ src/view/Home/components/Footer/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Icon from '@components/Icon'
-import { FooterContainer, FooterMedias } from './style'
+import { FooterContainer, FooterMedias, AnchorCask } from './style'
 import Image from '@components/Image'
 
 const Footer = () => {
@@ -14,9 +14,18 @@ const Footer = () => {
         containerY={62}
       />
       <FooterMedias>
-        <Icon id="instagram" height={32} width={32} />
-        <Icon id="whatsapp" height={32} width={32} />
-        <Icon id="facebook" height={32} width={32} />
+        <AnchorCask href="https://www.instagram.com/vrownievegano/">
+          <Icon id="instagram" height={32} width={32} />
+        </AnchorCask>
+
+        <AnchorCask href="https://api.whatsapp.com/send?phone=5511988256175">
+          <Icon id="whatsapp" height={32} width={32} />
+        </AnchorCask>
+
+        {/* Waiting for the current Facebook url or other link  */}
+        <AnchorCask href="https://web.facebook.com/vrownievegano/?_rdc=1&_rdr">
+          <Icon id="facebook" height={32} width={32} />
+        </AnchorCask>
       </FooterMedias>
     </FooterContainer>
   )
